7d9f4b1a9d5408353d236eb329d1e6b18b83dc81
Tatsiana1111 <tanyashket@gmail.com>
2023-01-08 23:34:56 +0300

    feat: add profile page css

--- src/ui/pages/ProfilePage/ProfilePage.tsx
+++ src/ui/pages/ProfilePage/ProfilePage.tsx
@@ -5,31 +5,58 @@ import message from '../../../common/assets/icons/message.svg'
 import music from '../../../common/assets/icons/music.svg'
 import profile from '../../../common/assets/icons/profile.svg'
 import users from '../../../common/assets/icons/users.svg'
+import avatar from '../../../common/assets/images/avatar.svg'
+import miniAvatar from '../../../common/assets/images/miniAvatar.svg'
+import postPhoto from '../../../common/assets/images/postPhoto.svg'
 
-import { MenuDiv } from './styled'
+import { AboutProfileDiv, AvatarDiv, MenuDiv, PostDiv } from './styled'
 
 export const ProfilePage = () => {
+  const onClickProfileHandler = () => {
+    console.log('hi')
+  }
+
   return (
     <div>
-      <MenuDiv>
-        <img alt="profile icon" src={profile} /> Профиль
-      </MenuDiv>
-      <MenuDiv>
-        <img alt="profile icon" src={message} />
-        Сообщения
-      </MenuDiv>
-      <MenuDiv>
-        <img alt="profile icon" src={users} />
-        Найти друзей
-      </MenuDiv>
-      <MenuDiv>
-        <img alt="profile icon" src={music} />
-        Музыка
-      </MenuDiv>
-      <MenuDiv>
-        <img alt="profile icon" src={community} />
-        Сообщества
-      </MenuDiv>
+      <div>
+        <MenuDiv onClick={onClickProfileHandler}>
+          <img alt="profile icon" src={profile} /> Профиль
+        </MenuDiv>
+        <MenuDiv>
+          <img alt="profile icon" src={message} />
+          Сообщения
+        </MenuDiv>
+        <MenuDiv>
+          <img alt="profile icon" src={users} />
+          Найти друзей
+        </MenuDiv>
+        <MenuDiv>
+          <img alt="profile icon" src={music} />
+          Музыка
+        </MenuDiv>
+        <MenuDiv>
+          <img alt="profile icon" src={community} />
+          Сообщества
+        </MenuDiv>
+      </div>
+      <AvatarDiv>
+        <img alt="user avatar" src={avatar} />
+      </AvatarDiv>
+      <AboutProfileDiv>
+        <span>Profile name</span>
+        <span>Status</span>
+      </AboutProfileDiv>
+      <PostDiv>
+        <div>
+          <img src={miniAvatar} alt="user mini-avatar" />
+          <span>Profile name</span>
+        </div>
+        <img src={postPhoto} alt="post photo" />
+        <div>
+          <img src={miniAvatar} alt="user mini-avatar" />
+          <textarea name="" id="" placeholder="Напишите свой комментарий"></textarea>
+        </div>
+      </PostDiv>
     </div>
   )
 }
--- src/ui/pages/ProfilePage/styled.tsx
+++ src/ui/pages/ProfilePage/styled.tsx
@@ -5,17 +5,77 @@ export const MenuDiv = styled.div`
     margin-right: 10px;
   }
 
+  cursor: pointer;
   margin: 10px;
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: flex-start;
   font-weight: bold;
+  font-family: 'Roboto Light', sans-serif;
   font-size: 20px;
   color: #1f3c60;
+
+  :active {
+    color: #526884;
+  }
 `
-export const MenuWrapper = styled.div`
+export const AvatarDiv = styled.div`
+  img {
+    width: 250px;
+    height: 250px;
+  }
+
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
   display: flex;
-  justify-content: space-between;
   align-items: center;
+  justify-content: center;
+  background-color: white;
+  width: 292px;
+  height: 292px;
+  border-radius: 10px;
+`
+export const AboutProfileDiv = styled.div`
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+  width: 572px;
+  height: 292px;
+  border-radius: 10px;
+  margin: 20px;
+
+  span {
+    margin: 20px;
+    border-radius: 10px;
+    font-family: 'Roboto Light', sans-serif;
+    font-size: 24px;
+    background-color: white;
+  }
+`
+export const PostDiv = styled.div`
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+  width: 572px;
+  height: 564px;
+  border-radius: 10px;
+
+  div {
+    background-color: white;
+    border-radius: 10px;
+  }
+
+  img {
+    background-color: white;
+  }
+
+  span {
+    margin: 20px;
+    border-radius: 10px;
+    font-family: 'Roboto Light', sans-serif;
+    font-size: 24px;
+    background-color: white;
+  }
 `
