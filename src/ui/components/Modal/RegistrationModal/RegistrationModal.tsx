1def5c717a65b62d20a94662565599cee86afa66
nazarPtyt <ockap90@libero.it>
2023-01-12 16:51:24 +0100

    feat(SignIn): add RegistrationModalWrapper with possibility to alert the Data from inputs

--- src/ui/components/Modal/RegistrationModal/RegistrationModal.tsx
+++ src/ui/components/Modal/RegistrationModal/RegistrationModal.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import { useForm } from 'react-hook-form'
 import styled from 'styled-components'
 
 import { SButtonGreen } from '../../Button/SButton'
@@ -18,24 +19,86 @@ const RegistrationModalWrapper = styled.form`
 `
 
 export const RegistrationModal = () => {
+   const {
+      register,
+      handleSubmit,
+      setError,
+      formState: { errors },
+   } = useForm({
+      defaultValues: {
+         name: '' as string,
+         surname: '' as string,
+         email: '' as string,
+         password: '' as string,
+         date: { day: 0 as number, month: 0 as number, year: 0 as number },
+         gender: 'mail' as 'mail' | 'female',
+      },
+   })
+
    return (
-      <RegistrationModalWrapper>
+      <RegistrationModalWrapper
+         onSubmit={handleSubmit((data: any) => {
+            alert(JSON.stringify(data, null, 2))
+         })}
+      >
          <div>
-            <SInput type={'text'} placeholder={'Name'} />
-            <SInput type={'text'} placeholder={'Surname'} />
+            <SInput
+               type={'text'}
+               placeholder={'Name'}
+               {...register('name', { required: 'Please enter your first name.' })}
+            />
+            <SInput
+               type={'text'}
+               placeholder={'Surname'}
+               {...register('surname', { required: 'Please enter your surname.' })}
+            />
          </div>
-         <SInput type={'email'} placeholder={'Email'} />
-         <SInput type={'password'} placeholder={'Password'} />
-         <label htmlFor="cheese">
-            <SInput type={'number'} min={1} max={31} placeholder={'Day'} />
-            <SInput type={'number'} min={1} max={12} placeholder={'Month'} />
-            <SInput type={'number'} min={1920} max={2020} placeholder={'Year'} />
+         <SInput
+            type={'email'}
+            placeholder={'Email'}
+            {...register('email', { required: 'Please enter your email' })}
+         />
+         <SInput
+            type={'password'}
+            placeholder={'Password'}
+            {...register('password', { required: 'Password is required' })}
+         />
+         <label htmlFor="date">
+            <SInput
+               type={'number'}
+               min={1}
+               max={31}
+               placeholder={'Day'}
+               {...register('date.day', { required: 'Please enter the day when you born' })}
+            />
+            <SInput
+               type={'number'}
+               min={1}
+               max={12}
+               placeholder={'Month'}
+               {...register('date.month', { required: 'Please enter the month when you born' })}
+            />
+            <SInput
+               type={'number'}
+               min={1920}
+               max={2020}
+               placeholder={'Year'}
+               {...register('date.year', { required: 'Please enter the year when you born' })}
+            />
          </label>
-         <label htmlFor="cheese">
-            <SInput type={'checkbox'} placeholder={'Man'} />
-            <SInput type={'checkbox'} placeholder={'Female'} />
+         <label>
+            <SInput
+               type={'radio'}
+               value="male"
+               {...register('gender', { required: 'Please enter the year when you born' })}
+            />
+            <SInput
+               type={'radio'}
+               value="female"
+               {...register('gender', { required: 'Please enter the year when you born' })}
+            />
          </label>
-         <SButtonGreen>Register Me</SButtonGreen>
+         <SButtonGreen type="submit">Register Me</SButtonGreen>
       </RegistrationModalWrapper>
    )
 }
