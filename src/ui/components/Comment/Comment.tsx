091b1e27191c46014f280a0aa31373011caa057b
Tatsiana1111 <tanyashket@gmail.com>
2023-01-24 16:38:50 +0300

    feat(Profile): add comments style

--- src/ui/components/Comment/Comment.tsx
+++ src/ui/components/Comment/Comment.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
+import userPhoto from '../../../common/images/defaultUser.jpg'
 import { CommentsDataType, PostDataType } from '../../../dal/profileAPI'
+
+import { CommentEmail, CommentText, PostDiv } from './styled'
 
 type CommentPropsType = {
    comments: CommentsDataType[]
@@ -8,12 +11,20 @@ type CommentPropsType = {
 }
 export const Comment = (props: CommentPropsType) => {
    return (
-      <div>
+      <>
          {props.comments.map(comment => {
             if (props.post.id === comment.postId) {
-               return <div key={comment.id}>{comment.body}</div>
+               return (
+                  <PostDiv key={comment.id}>
+                     <img src={userPhoto} alt="post user photo" />
+                     <div>
+                        <CommentEmail>{comment.email}</CommentEmail>
+                        <CommentText key={comment.id}>{comment.body}</CommentText>
+                     </div>
+                  </PostDiv>
+               )
             }
          })}
-      </div>
+      </>
    )
 }
