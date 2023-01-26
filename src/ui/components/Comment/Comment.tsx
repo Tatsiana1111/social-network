9e0087f8ea9589f7901a16c3c93e7ddd524dbebd
nazarPtyt <ockap90@libero.it>
2023-01-26 14:00:18 +0100

    fix(Comment): show user photo when new comment was added

--- src/ui/components/Comment/Comment.tsx
+++ src/ui/components/Comment/Comment.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-import userPhoto from '../../../common/images/defaultUser.jpg'
+import { useAppSelector } from '../../../app/hooks'
+import defaultUser from '../../../common/images/defaultUser.jpg'
 import { CommentsDataType } from '../../../dal/profileAPI'
 
 import { CommentEmail, CommentText, CommentWrapper } from './styled'
@@ -9,9 +10,15 @@ type CommentPropsType = {
    comment: CommentsDataType
 }
 export const Comment = (props: CommentPropsType) => {
+   const userPhoto = useAppSelector(state => state.profile.data.photos.large)
+   const userName = useAppSelector(state => state.profile.data.fullName)
+
    return (
       <CommentWrapper>
-         <img src={userPhoto} alt="post user photo" />
+         <img
+            src={props.comment.name === userName ? userPhoto : defaultUser}
+            alt="post user photo"
+         />
          <div>
             <CommentEmail>{props.comment.name}</CommentEmail>
             <CommentText>{props.comment.body}</CommentText>
