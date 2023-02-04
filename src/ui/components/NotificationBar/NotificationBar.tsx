7d27a8dbb74906ae6d4b9baa48367f195092acdd
nazarPtyt <ockap90@libero.it>
2023-02-04 13:40:33 +0100

    feat(NotificationBar): add CloseModal button

--- src/ui/components/NotificationBar/NotificationBar.tsx
+++ src/ui/components/NotificationBar/NotificationBar.tsx
@@ -11,6 +11,40 @@ const NotificationBarWrapper = styled.div`
    height: 100px;
    background-color: forestgreen;
    border-radius: 20px;
+   margin: 0 0 10px 10px;
+`
+const CloseModal = styled.span`
+   position: absolute;
+   top: 10px;
+   right: 10px;
+   width: 24px;
+   height: 24px;
+   opacity: 0.5;
+   cursor: pointer;
+   transition: opacity ease 0.5s;
+
+   &:hover {
+      opacity: 1;
+   }
+
+   ::before,
+   ::after {
+      content: '';
+      position: absolute;
+      top: 10px;
+      display: block;
+      width: 24px;
+      height: 3px;
+      background: #000;
+   }
+
+   ::before {
+      transform: rotate(45deg);
+   }
+
+   ::after {
+      transform: rotate(-45deg);
+   }
 `
 
 type PropsType = {
@@ -21,7 +55,7 @@ export const NotificationBar = (props: PropsType) => {
       <NotificationBarWrapper>
          <div>
             <h2>{props.text}</h2>
-            <span></span>
+            <CloseModal />
          </div>
       </NotificationBarWrapper>
    )
