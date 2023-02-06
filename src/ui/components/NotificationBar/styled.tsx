91e8c3de96ab1e6e4df8c694b36440aa0ec1e900
nazarPtyt <ockap90@libero.it>
2023-02-06 22:08:45 +0100

    fix(NotificationBar): works properly, need to fix styles

--- src/ui/components/NotificationBar/styled.tsx
+++ src/ui/components/NotificationBar/styled.tsx
@@ -1,5 +1,67 @@
-import styled from 'styled-components'
-export const ProgressBar = styled.div``
+import styled, { keyframes } from 'styled-components'
+
+const SlideRight = keyframes`
+  from {
+    margin-left: -120%;
+  }
+
+  to {
+    margin-left: 0;
+  }
+`
+const SlideLeft = keyframes`
+  from {
+    margin-left: 0;
+  }
+
+  to {
+    left: -120%;
+  }
+`
+
+type D = {
+   exit?: boolean
+   success?: boolean
+   error?: boolean
+}
+
+export const NotificationBarWrapper = styled.div<D>`
+   position: fixed;
+   bottom: 0;
+   left: 0;
+   width: 300px;
+   margin-left: 10px;
+   margin-bottom: 10px;
+   background-color: ${props => (props.error ? 'red' : 'geed')};
+   animation: ${props => (props.exit ? SlideLeft : SlideRight)} 0.4s;
+   animation-fill-mode: forwards;
+`
+
+export const NotificationBarItem = styled.div`
+   width: 100%;
+
+   border-radius: 20px;
+   overflow: hidden;
+
+   div {
+      //display: flex;
+      //align-items: center;
+      //justify-content: center;
+      //height: 100%;
+
+      p {
+         margin: 0;
+         padding: 10px;
+         text-align: center;
+      }
+   }
+`
+
+export const ProgressBar = styled.div`
+   background-color: #65d266;
+   height: 5px;
+`
+
 export const CloseModalIcon = styled.span`
    position: absolute;
    top: 10px;
