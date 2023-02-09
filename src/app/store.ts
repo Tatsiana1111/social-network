9aeeb140c4d67765ce95a51b938ca2ac8fdcac5e
nazarPtyt <ockap90@libero.it>
2023-02-09 13:12:43 +0100

    localStorageMiddleware

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, Dispatch, Action } from '@reduxjs/toolkit'
 
 import { albumsReducer } from '../bll/albumsReducer'
 import { appReducer } from '../bll/appReducer'
 import { authReducer } from '../bll/authReducer'
 import { commentsReducer } from '../bll/commentsReducer'
-import { profileReducer } from '../bll/profileReducer'
+import { asyncProfileActions, profileReducer } from '../bll/profileReducer'
 import { usersReducer } from '../bll/usersReducer'
+//https://www.appsloveworld.com/reactjs/100/30/local-storage-using-redux-toolkit
 
-// const reHydrateStore = () => {
-//    if (localStorage.getItem('theme') !== null) {
-//       return JSON.parse(JSON.stringify(localStorage.getItem('theme'))) // re-hydrate the store
-//    }
-// }
+const localStorageMiddleware = (getState: any) => {
+   return (next: any) => (action: any) => {
+      const result = next(action)
+
+      localStorage.setItem('appState', JSON.stringify(getState()))
+
+      return result
+   }
+}
+
+const reHydrateStore = () => {
+   if (localStorage.getItem('appState') !== null) {
+      return JSON.parse(JSON.stringify(localStorage.getItem('appState'))) // re-hydrate the store
+   }
+}
 
 export const store = configureStore({
    reducer: {
@@ -22,6 +33,8 @@ export const store = configureStore({
       comments: commentsReducer,
       albums: albumsReducer,
    },
+   preloadedState: reHydrateStore(),
+   middleware: getDefaultMiddleware => getDefaultMiddleware().concat(localStorageMiddleware),
 })
 
 export type RootState = ReturnType<typeof store.getState>
