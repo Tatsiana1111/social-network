03a91a2b287cb6695961a31e0a64cf196cc44c86
nazarPtyt <ockap90@libero.it>
2023-02-11 12:23:08 +0100

    feat(authReducer): add error handler to thunks

--- src/bll/authReducer.ts
+++ src/bll/authReducer.ts
@@ -1,5 +1,7 @@
 import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { AxiosError } from 'axios'
 
+import { HandleServerAppError, HandleServerNetworkError } from '../common/Utils/errorHandler'
 import { authAPI, LoginRequestDataType } from '../dal/authAPI'
 
 const initialState = {
@@ -22,27 +24,35 @@ export const authReducer = authSlice.reducer
 // Thunk
 export const LoginTC = createAsyncThunk(
    'auth/login',
-   async (data: LoginRequestDataType, thunkAPI) => {
+   async (data: LoginRequestDataType, { dispatch }) => {
       try {
          const res = await authAPI.login(data)
 
          if (res.data.resultCode === 0) {
-            thunkAPI.dispatch(setLoggedIn({ value: true }))
+            dispatch(setLoggedIn({ value: true }))
+         } else {
+            HandleServerAppError(dispatch, res.data)
          }
       } catch (e) {
-         console.log(e)
+         const error = e as AxiosError | Error
+
+         HandleServerNetworkError(dispatch, error)
       }
    }
 )
 
-export const LogOutTC = createAsyncThunk('auth/logOut', async (arg, thunkAPI) => {
+export const LogOutTC = createAsyncThunk('auth/logOut', async (arg, { dispatch }) => {
    try {
       const res = await authAPI.logOut()
 
       if (res.data.resultCode === 0) {
-         thunkAPI.dispatch(setLoggedIn({ value: false }))
+         dispatch(setLoggedIn({ value: false }))
+      } else {
+         HandleServerAppError(dispatch, res.data)
       }
    } catch (e) {
-      console.log(e)
+      const error = e as AxiosError | Error
+
+      HandleServerNetworkError(dispatch, error)
    }
 })
