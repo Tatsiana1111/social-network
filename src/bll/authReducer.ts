0e496bf7c25fcc15c9806eb5ecfcddc856c42c7a
Tatsiana1111 <tanyashket@gmail.com>
2023-02-25 20:49:43 +0300

    feat(Auth): add captcha

--- src/bll/authReducer.ts
+++ src/bll/authReducer.ts
@@ -8,6 +8,7 @@ import { initializeAppTC, setAppStatusAC } from './appReducer'
 
 const initialState = {
    isLoggedIn: false as boolean,
+   captcha: null as null | string,
 }
 
 export const authSlice = createSlice({
@@ -17,10 +18,13 @@ export const authSlice = createSlice({
       setLoggedIn: (state, action: PayloadAction<{ value: boolean }>) => {
          state.isLoggedIn = action.payload.value
       },
+      getCaptchaSuccess: (state, action: PayloadAction<string | null>) => {
+         state.captcha = action.payload
+      },
    },
 })
 
-export const { setLoggedIn } = authSlice.actions
+export const { setLoggedIn, getCaptchaSuccess } = authSlice.actions
 export const authReducer = authSlice.reducer
 
 // Thunk
@@ -36,6 +40,9 @@ export const LoginTC = createAsyncThunk(
             dispatch(setAppStatusAC({ status: 'success' }))
             dispatch(initializeAppTC())
          } else {
+            if (res.data.resultCode === 10) {
+               dispatch(getCaptcha())
+            }
             HandleServerAppError(dispatch, res.data)
          }
       } catch (e) {
@@ -55,6 +62,19 @@ export const LogOutTC = createAsyncThunk('auth/logOut', async (arg, { dispatch }
       } else {
          HandleServerAppError(dispatch, res.data)
       }
+   } catch (e) {
+      const error = e as AxiosError | Error
+
+      HandleServerNetworkError(dispatch, error)
+   }
+})
+
+export const getCaptcha = createAsyncThunk('auth/captcha', async (arg, { dispatch }) => {
+   try {
+      const res = await authAPI.captcha()
+      const captchaUrl = res.data.url
+
+      dispatch(getCaptchaSuccess(captchaUrl))
    } catch (e) {
       const error = e as AxiosError | Error
 
